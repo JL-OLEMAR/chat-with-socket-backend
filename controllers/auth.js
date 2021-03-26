d98b23d6f7a05f8620c34f6f8495cde5b5456077
JL-OLEMAR <joseluis19963@gmail.com>
2021-03-26 02:19:56 -0500

    Encriptar la contraseña del usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,38 @@
 const { response } = require('express')
+const bcrypt = require('bcryptjs')
+const Usuario = require('../models/usuario')
 
 const crearUsuario = async (req, res = response) => {
-  const { nombre, email, password } = req.body
-  res.json({
-    ok: true,
-    usuario: {
-      nombre,
-      email,
-      password
+  try {
+    const { email, password } = req.body
+
+    // verificar que el email no exista
+    const existeEmail = await Usuario.findOne({ email })
+    if (existeEmail) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El correo ya existe'
+      })
     }
-  })
+
+    const usuario = new Usuario(req.body)
+    // Encriptar contraseña
+    const salt = bcrypt.genSaltSync()
+    usuario.password = bcrypt.hashSync(password, salt)
+
+    // Guardar usuario en DB
+    await usuario.save()
+
+    res.json({
+      usuario
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador'
+    })
+  }
 }
 
 const login = async (req, res = response) => {
